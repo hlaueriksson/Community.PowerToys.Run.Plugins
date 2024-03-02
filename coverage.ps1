06444d7c9c4b4ce8666ff2082e84c44d7c37b508
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2024-03-02 21:17:57 +0100

    coverage

---
+++ coverage.ps1
@@ -0,0 +1,9 @@
+# Clean
+Get-ChildItem -Path ".\src" -Directory -Include "bin", "obj" -Recurse | Remove-Item -Recurse -Force
+Remove-Item -LiteralPath "TestResults" -Recurse -Force
+
+# Test
+dotnet test --collect "Code Coverage" --settings coverage.runsettings /p:Platform=x64
+
+# Report
+reportgenerator -reports:"src\**\coverage.xml" -targetdir:"TestResults\Coverage" -reporttypes:Html -filefilters:"-*RegexGenerator.g.cs;-*Moq*;-*mockhttp*"
